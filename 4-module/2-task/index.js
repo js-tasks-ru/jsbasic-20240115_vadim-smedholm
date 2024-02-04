d7652d2fe73c835a2e0e4505ea7289783f18b961
Vadim Smedholm <77298668+Orava1973@users.noreply.github.com>
2024-02-04 18:37:50 +0200

    solving three tasks from module 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,12 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  // Getting all rows of the table
+  const rows = table.rows;
+  // We go through the rows and highlight the cells diagonally in red
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    // We get the cell diagonally in the current row
+    const diagonalCell = row.cells[i];
+    // Highlight the cell in red
+    diagonalCell.style.backgroundColor = 'red';
+  }// ваш код..
 }
